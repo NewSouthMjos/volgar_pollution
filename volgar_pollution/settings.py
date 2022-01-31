6defcdb235b4a4bba057ce60daa4c09e0c586ba9
Evgeny <mjos1995@gmail.com>
2022-01-31 14:54:57 +0400

    changes for heroku

--- volgar_pollution/settings.py
+++ volgar_pollution/settings.py
@@ -128,13 +128,18 @@ TIME_ZONE = 'Europe/Samara'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(os.path.dirname(__file__), 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
