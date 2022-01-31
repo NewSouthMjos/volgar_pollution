b272138bdf18062ddb8c129b1f4a6ef75c7d11de
Evgeny <mjos1995@gmail.com>
2022-01-31 16:19:52 +0400

    heroku changes

--- volgar_pollution/apps/pollution_app/apps.py
+++ volgar_pollution/apps/pollution_app/apps.py
@@ -9,13 +9,13 @@ class PollutionAppConfig(AppConfig):
     name = 'pollution_app'
 
     def ready(self):
-        
+        pass
         # Запуск собрания данных по расписанию
         # from pollution_app.services import data_update
         # data_update.start()
 
         # Запуск инициализации начальных значений в БД
         # по расшифровке загрязнений, Impurity
-        from pollution_app.services.requests_pogodasv import check_impurities
-        not_in_bd_impurities = check_impurities()
-        logger.info(f'Добавлены данные в таблицу Impurity: {not_in_bd_impurities}')
+        # from pollution_app.services.requests_pogodasv import check_impurities
+        # not_in_bd_impurities = check_impurities()
+        # logger.info(f'Добавлены данные в таблицу Impurity: {not_in_bd_impurities}')
