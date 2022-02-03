919b4a4c9f3af9996bf8d41e9f9ea9aee9116a49
EvgenyNetVision <mjos1995@gmail.com>
2022-02-03 15:55:35 +0500

    debug

--- volgar_pollution/apps/pollution_app/services/data_update.py
+++ volgar_pollution/apps/pollution_app/services/data_update.py
@@ -1,28 +1,29 @@
-# from datetime import datetime
-# import logging
+from datetime import datetime
+import logging
 
 # from apscheduler.schedulers.background import BackgroundScheduler
-# # from pollution_app.services.requests_pogodasv import new_data_point
-
-# logger = logging.getLogger(__name__)
-
-# def start():
-#     scheduler = BackgroundScheduler()
-#     # scheduler.add_job(new_data_point, 'cron', minute='10,30,50')
-#     scheduler.add_job(test_job, 'interval', minutes=1)
-#     scheduler.start()
-
-# def test_job():
-#     logger.info('Executed test job done!')
-
-# start()
-
 from apscheduler.schedulers.blocking import BlockingScheduler
+# from pollution_app.services.requests_pogodasv import new_data_point
 
-sched = BlockingScheduler()
+logger = logging.getLogger(__name__)
 
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every three minutes.')
+def start():
+    scheduler = BlockingScheduler()
+    # scheduler.add_job(new_data_point, 'cron', minute='10,30,50')
+    scheduler.add_job(test_job, 'interval', minutes=1)
+    scheduler.start()
 
-sched.start()
+def test_job():
+    logger.info('Executed test job done!')
+
+start()
+
+# from apscheduler.schedulers.blocking import BlockingScheduler
+
+# sched = BlockingScheduler()
+
+# @sched.scheduled_job('interval', minutes=1)
+# def timed_job():
+#     print('This job is run every three minutes.')
+
+# sched.start()
