6905059dc93d8391851098d7f58ed5886b9a59cb
EvgenyNetVision <mjos1995@gmail.com>
2022-02-03 15:19:15 +0500

    add clock process

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python manage.py migrate
 web: gunicorn volgar_pollution.wsgi --log-file -
+clock: python volgar_pollution/apps/pollution_app/services/data_update.py
--- volgar_pollution/apps/pollution_app/services/data_update.py
+++ volgar_pollution/apps/pollution_app/services/data_update.py
@@ -2,7 +2,15 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from pollution_app.services.requests_pogodasv import new_data_point
 
+logger = logging.getLogger(__name__)
+
 def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(new_data_point, 'cron', minute='10,30,50')
+    scheduler.add_job(test_job, 'cron', minute='*')
     scheduler.start()
+
+def test_job():
+    logger.info('Executed test job done!')
+
+start()
