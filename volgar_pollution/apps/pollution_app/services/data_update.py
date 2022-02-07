3b6a4bb114cc3f7d404efd3d354dc3194afcc3b3
EvgenyNetVision <mjos1995@gmail.com>
2022-02-07 11:23:43 +0400

    heroku changes

--- volgar_pollution/apps/pollution_app/services/data_update.py
+++ volgar_pollution/apps/pollution_app/services/data_update.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 import logging
 
-# from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 # from pollution_app.services.requests_pogodasv import new_data_point
 
 logger = logging.getLogger(__name__)
 
 def start():
-    scheduler = BlockingScheduler()
+    scheduler = BackgroundScheduler()
     # scheduler.add_job(new_data_point, 'cron', minute='10,30,50')
     scheduler.add_job(test_job, 'interval', minutes=1)
     scheduler.start()
